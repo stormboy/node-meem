8301c916353e3ee99daddb842fe97c4884775949
stormboy <stormboy@stormboy.com>
2013-12-06 10:05:10 +1100

    fixed module path for meem.

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ exports.MeemServer = require("./lib/meemserver");
 exports.MeemStore = require("./lib/meemstore");
 
 exports.meems = {
-	hyperspace: require("./lib/meems/hyperspace"),
-	demo: require("./lib/meems/basic"),
+	core: require("./lib/meems/core"),
+	demo: require("./lib/meems/demo"),
 };
